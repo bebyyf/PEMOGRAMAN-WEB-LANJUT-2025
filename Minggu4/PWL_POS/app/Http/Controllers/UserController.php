12baf43a21d883f346235b7acc39b25e78b1ea89
bebyyf <bebifindia@gmail.com>
2025-03-06 13:21:44 +0700

    Jobsheet 4

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -4,19 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
+        // tambah data user dengan Eloquent Model
         $data = [
-            'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' => 'Manager 2',
-            'password' => Hash::make('1234')
+            'nama' => 'Pelanggan Pertama',
         ];
-        UserModel::create($data);
         UserModel::where('username', 'customer-1')->update($data); // update data user
 
         // coba akses model UserModel
