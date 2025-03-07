c200aa5d96ccf060de079ede299dc0fb164402b9
bebyyf <bebifindia@gmail.com>
2025-03-07 09:11:21 +0700

    Jobsheet 4

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
         // UserModel::where('username', 'customer-1')->update($data); // update data user
 
         // coba akses model UserModel
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+        $jumlah_pengguna = UserModel::where('level_id', 2)->count();
+        return view('user', ['jumlah_pengguna' => $jumlah_pengguna]);
     }
 }
