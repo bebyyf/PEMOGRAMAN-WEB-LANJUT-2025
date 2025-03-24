745c18a66d9435832d7342c3b05dbbd130f5830f
bebyyf <bebifindia@gmail.com>
2025-03-25 04:40:02 +0700

    Implementasi JS 5

--- POS/resources/views/layouts/template.blade.php
+++ POS/resources/views/layouts/template.blade.php
@@ -22,8 +22,8 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ url('/') }}" class="brand-link">
-        <img src="{{ asset('adminlte/dist/img/AdminiTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img">
+    <a href="{{ url('/')}}" class="brand-link">
+        <img src="assets('adminlte/dist/img/AdminLTELogo.png')" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">PWL Starter Code</span>
     </a>
 
--- POS/routes/web.php
+++ POS/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,6 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::post('/user/ubah_simpan', [UserController::class, 'ubah_simpan']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+
+Route::get('/', [WelcomeController::class, 'index']);
