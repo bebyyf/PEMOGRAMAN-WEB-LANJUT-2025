a544e4f6dcf2c843533101f5774a846419dedf2b
bebyyf <bebifindia@gmail.com>
2025-04-09 16:45:56 +0700

    praktikum 3 JS 6

--- Minggu6/PWL_POS/routes/web.php
+++ Minggu6/PWL_POS/routes/web.php
@@ -53,6 +53,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']); // Menampilkan detail user menggunakan AJAX
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // Menampilkan halaman form edit user Ajax
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // Menyimpan perubahan data user Ajax
+     Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete user Ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // Untuk hapus data user Ajax
     Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
 });
 
