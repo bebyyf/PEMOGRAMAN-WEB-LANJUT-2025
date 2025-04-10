acba70f5ae3781359313ee5176797d3157c67ed4
bebyyf <bebifindia@gmail.com>
2025-04-10 15:50:37 +0700

    Tugas praktikum kategori

--- Minggu6/PWL_POS/routes/web.php
+++ Minggu6/PWL_POS/routes/web.php
@@ -69,21 +69,28 @@ Route::group(['prefix' => 'level'], function () {
     Route::get('/{id}/edit',[LevelController::class, 'edit']);
     Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']); 
     Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']); 
-    Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']); 
+    Route::get('/{id}/show_ajax', [LevelController::class, 'show_ajax']); 
     Route::delete('/{id}',[LevelController::class, 'destroy']);
     Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']); 
     Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']);
 });
 
 Route::group(['prefix' => 'kategori'], function () {
-    Route::get('/',[KategoriController::class, 'index']);
-    Route::post('/list',[KategoriController::class, 'list']);
-    Route::get('/create',[KategoriController::class, 'create']);
-    Route::post('/',[KategoriController::class, 'store']);
-    Route::get('/{id}',[KategoriController::class, 'show']);
-    Route::get('/{id}/edit',[KategoriController::class, 'edit']);
-    Route::put('/{id}',[KategoriController::class, 'update']);
-    Route::delete('/{id}',[KategoriController::class, 'destroy']);
+    Route::get('/', [KategoriController::class, 'index']); 
+    Route::post("/list", [KategoriController::class, 'list']); 
+    Route::get('/create', [KategoriController::class, 'create']); 
+    Route::post('/', [KategoriController::class, 'store']);  
+    Route::get('/create_ajax', [KategoriController::class, 'create_ajax']); 
+    Route::post('/ajax', [KategoriController::class, 'store_ajax']); 
+    Route::get('/{id}', [KategoriController::class, 'show']); 
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']); 
+    Route::put('/{id}', [KategoriController::class, 'update']); 
+    Route::get('/{id}/edit_ajax', [KategoriController::class, 'edit_ajax']);
+    Route::put('/{id}/update_ajax', [KategoriController::class, 'update_ajax']); 
+    Route::get('/{id}/delete_ajax', [KategoriController::class, 'confirm_ajax']); 
+    Route::delete('/{id}/delete_ajax', [KategoriController::class, 'delete_ajax']); 
+    Route::get('/{id}/show_ajax', [KategoriController::class, 'show_ajax']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']); 
 });
 
 Route::group(['prefix' => 'supplier'], function () {
