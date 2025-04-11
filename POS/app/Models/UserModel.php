9e139ccd2200d97467245d1704dcfd5efc710b10
bebyyf <bebifindia@gmail.com>
2025-04-11 21:20:32 +0700

    implementasi praktikum 3 JS 7

--- POS/app/Models/UserModel.php
+++ POS/app/Models/UserModel.php
@@ -20,21 +20,31 @@ class UserModel extends Authenticatable
     /**
      * Relasi ke tabel level
      */
-    public function level(): BelongsTo{
+    public function level(): BelongsTo
+    {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
     /**
      * Mendapatkan nama role
      */
-    public function getRoleName(): string{
+    public function getRoleName(): string
+    {
         return $this->level->level_nama;
     }
 
     /**
      * Cek apakah user memiliki role tertentu
      */
-    public function hasRole($role): bool{
+    public function hasRole($role): bool
+    {
         return $this->level->level_kode == $role;
+    }
+
+    /**
+     * Mendapatkan kode role
+     */
+    public function getRole(){
+        return $this->level->level_kode;
     }
 }
