c137256aa2e4568db0e7802a0841d9d28d25ded8
bebyyf <bebifindia@gmail.com>
2025-04-14 12:36:15 +0700

    Implementasi Praktikum 3 JS 8

--- POS/app/Http/Controllers/BarangController.php
+++ POS/app/Http/Controllers/BarangController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Hash;
 use Yajra\DataTables\Facades\DataTables;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Illuminate\Support\Facades\Validator;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class BarangController extends Controller
 {
@@ -417,4 +418,22 @@ class BarangController extends Controller
         $writer->save('php://output');
         exit;
     } // end function export_excel
+
+    // fungsi export_pdf() pada BarangController.php
+    public function export_pdf()
+    {
+        $barang = BarangModel::select('kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual')
+            ->orderBy('kategori_id')
+            ->orderBy('barang_kode')
+            ->with('kategori')
+            ->get();
+
+        // use Barryvdh\DomPDF\Facade\Pdf;
+        $pdf = Pdf::loadView('barang.export_pdf', ['barang' => $barang]);
+        $pdf->setPaper('a4', 'portrait'); // set ukuran kertas dan orientasi
+        $pdf->setOption("isRemoteEnabled", true); // set true jika ada gambar dari url
+        $pdf->render();
+
+        return $pdf->stream('Data Barang ' . date('Y-m-d H:i:s') . '.pdf');
+    }
 }
