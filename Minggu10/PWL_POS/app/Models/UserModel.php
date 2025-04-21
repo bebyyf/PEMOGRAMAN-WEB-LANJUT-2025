19139121640d6a3229ab631fa65b18eb3fb0c9ca
bebyyf <bebifindia@gmail.com>
2025-04-21 20:23:51 +0700

    Praktikum 1 Jobsheet 10

--- Minggu10/PWL_POS/app/Models/UserModel.php
+++ Minggu10/PWL_POS/app/Models/UserModel.php
@@ -5,13 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['username', 'password', 'nama', 'level_id', 'user_profile_picture'];
